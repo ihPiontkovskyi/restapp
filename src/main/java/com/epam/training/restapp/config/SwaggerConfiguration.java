3a05dc0cd0128a32c500abdb14a24ae7ef7685bc
ihPiontkovskyi <fuvkaperviy@gmail.com>
2020-05-20 20:18:11 +0300

    Add swagger documentation

---
+++ src/main/java/com/epam/training/restapp/config/SwaggerConfiguration.java
@@ -0,0 +1,34 @@
+package com.epam.training.restapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.
+                        basePackage("com.epam.training.restapp.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("REST API training project")
+                .description("Users and posts REST API")
+                .contact(new Contact("Ihor Piontkovskyi", "https://github.com/ihPiontkovskyi", "piontkowski1109@gmail.com"))
+                .version("0.0.1")
+                .build();
+    }
+}
